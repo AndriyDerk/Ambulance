46e6f04fbef2cadc115f9a38714ce934530aed99
Andriy Derkach <andriyderk@gmail.com>
2022-08-09 16:10:22 +0300

    update router.index

--- server/router/index.js
+++ server/router/index.js
@@ -1,13 +1,13 @@
 const express = require('express'),
      router = new express() ,
      userRouter = require('./userRouter'),
-     //illnessRouter = require('./illnessRouter'),
+     illnessRouter = require('./illnessRouter'),
      doctorRouter = require('./doctorRouter');
-    // medicalHistoryRouter = require('./medicalHistoryRouter');
+     medicalHistoryRouter = require('./medicalHistoryRouter');
 
 router.use('/user', userRouter)
-// router.use('/illness', illnessRouter)
+router.use('/illness', illnessRouter)
 router.use('/doctor', doctorRouter)
-// router.use('/mhistory', medicalHistoryRouter)
+router.use('/mhistory', medicalHistoryRouter)
 
 module.exports = router
